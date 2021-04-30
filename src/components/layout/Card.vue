48afd37ed9becabe2509f3a4779b2d8e33c018f1
Martijn Faber <martijn@fabtronics.nl>
2021-04-30 15:15:08 +0200

    feat(card): added box shadow

--- src/components/layout/Card.vue
+++ src/components/layout/Card.vue
@@ -9,5 +9,8 @@
   padding: 20px;
   border-radius: 25px;
   background: var(--body-lighter);
+  -webkit-box-shadow: 0px 10px 50px -10px rgba(0, 0, 0, 0.5);
+  -moz-box-shadow: 0px 10px 50px -10px rgba(0, 0, 0, 0.5);
+  box-shadow: 0px 10px 50px -10px rgba(0, 0, 0, 0.5);
 }
 </style>
