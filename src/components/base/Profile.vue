bf342b77ccfa9183f2226aa082c70fe7ec980fc4
Jip Fr <jipfrijlink@gmail.com>
2021-04-30 14:34:30 +0200

    refactor(user): move profile into new component

---
+++ src/components/base/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="profile">
+    <img
+      src="https://i.scdn.co/image/ab6775700000ee85491b935efc2385031a2fa18e"
+      class="profile-picture"
+      alt
+    />
+    <div class="profile-info">
+      <h2 class="profile-name">Sjoerd</h2>
+      <p class="profile-bio">shi daggoetje geswiped</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.profile {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .profile-picture {
+    border-radius: 100%;
+    height: 200px;
+    width: 200px;
+    object-fit: cover;
+  }
+}
+
+.profile-info {
+  margin-top: var(--gap-large);
+  text-align: center;
+
+  .profile-name {
+    font-size: 2rem;
+  }
+
+  .profile-bio {
+    font-size: 1.5rem;
+  }
+}
+</style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -2,17 +2,7 @@
   <div id="user">
     <!-- Top section with profile -->
     <div class="block">
-      <div class="profile">
-        <img
-          src="https://i.scdn.co/image/ab6775700000ee85491b935efc2385031a2fa18e"
-          class="profile-picture"
-          alt
-        />
-        <div class="profile-info">
-          <h2 class="profile-name">Sjoerd</h2>
-          <p class="profile-bio">shi daggoetje geswiped</p>
-        </div>
-      </div>
+      <Profile />
     </div>
     <!-- Main page -->
     <main>
@@ -75,33 +65,6 @@
   padding-bottom: 150px;
 }
 
-.profile {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  .profile-picture {
-    border-radius: 100%;
-    height: 200px;
-    width: 200px;
-    object-fit: cover;
-  }
-}
-
-.profile-info {
-  margin-top: var(--gap-large);
-  text-align: center;
-
-  .profile-name {
-    font-size: 2rem;
-  }
-
-  .profile-bio {
-    font-size: 1.5rem;
-  }
-}
-
 main {
   margin-top: -100px;
 }
@@ -134,6 +97,7 @@ main {
 import { defineComponent } from 'vue';
 
 // Import components
+import Profile from '../components/base/Profile.vue';
 import Container from '../components/layout/Container.vue';
 import Card from '../components/layout/Card.vue';
 import ImageCardContainer from '../components/layout/ImageCardContainer.vue';
@@ -141,6 +105,7 @@ import ImageCard from '../components/base/ImageCard.vue';
 
 export default defineComponent({
   components: {
+    Profile,
     Container,
     Card,
     ImageCardContainer,
