00b1ea2cb2b7369de13941ce72664e47a6af86e0
Jip Fr <jipfrijlink@gmail.com>
2021-04-30 13:11:02 +0200

    feat(components): image cards for albums/other

---
+++ src/components/base/ImageCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <article class="image-card">
+    <div class="square-img">
+      <img class="main-image" :src="image" />
+    </div>
+    <div class="card-info">
+      <h2 class="title">{{ title }}</h2>
+    </div>
+  </article>
+</template>
+
+<style lang="scss" scoped>
+.image-card {
+  width: 100%;
+}
+
+// The image HAS to be 1:1
+// I would normally use a background image, but background images don't allow alt texts
+.square-img {
+  width: 100%;
+  padding-bottom: 100%;
+  position: relative;
+
+  > img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+}
+
+.main-image {
+  border-radius: 25px;
+}
+
+.card-info {
+  margin-top: var(--gap);
+
+  .title {
+    font-size: 1rem;
+    width: 100%;
+    text-align: center;
+  }
+
+}
+
+</style>
+
+<script lang="ts">
+// Import Vue
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,38 @@
 <template>
-  <div id="home">
+  <div class="home">
     <Container>
       Hello world
+      <div class="tmp">
+        <ImageCard
+          image="https://i0.wp.com/abitofpopmusic.com/wp-content/uploads/2020/06/astrid-s-dance-dance-dance.jpg?fit=1200%2C1200&ssl=1"
+          title="Astrid S"
+        />
+        <ImageCard
+          image="https://i0.wp.com/abitofpopmusic.com/wp-content/uploads/2020/06/astrid-s-dance-dance-dance.jpg?fit=1200%2C1200&ssl=1"
+          title="Dance Dance Dance"
+        />
+        <ImageCard
+          image="https://upload.wikimedia.org/wikipedia/en/2/26/Dark_Was_the_Night_%28Front_Cover%29.png"
+          title="So Far Around The Bend"
+        />
+      </div>
     </Container>
   </div>
 </template>
 
 <style lang="scss" scoped>
-#home {
+.home {
   margin: 80px 0;
+}
+.tmp {
+  // This is a temporary element + layout.
+  // I repeat: TEMPORARY!
+  background: var(--body-lighter);
+  padding: 20px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
+  grid-gap: var(--gap-large);
 }
 </style>
 
@@ -18,10 +42,12 @@ import { defineComponent } from 'vue';
 
 // Import components
 import Container from '../components/layout/Container.vue';
+import ImageCard from '../components/base/ImageCard.vue';
 
 export default defineComponent({
   components: {
     Container,
+    ImageCard,
   },
 });
 </script>
