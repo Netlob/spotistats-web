4d4d4d119bfc77b84129bac2de5214e131ce522a
Martijn Faber <martijn@fabtronics.nl>
2021-04-30 20:31:46 +0200

    feat(Signin): add signin view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
 import User from '../views/User.vue';
+import Signin from '../views/Signin.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -12,6 +13,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/user/:id',
     name: 'User',
     component: User,
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    component: Signin,
   },
   {
     path: '/about',
---
+++ src/views/Signin.vue
@@ -0,0 +1,48 @@
+<template>
+  <Container>
+    <div class="card-wrapper">
+      <Card class="card">
+        <Qrcode
+          value="https://spotistats.app"
+          background="transparent"
+          foreground="#1ED761"
+          level="H"
+          size="180"
+        ></Qrcode>
+        <Button>Sign in with Spotify</Button>
+      </Card>
+    </div>
+  </Container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Qrcode from 'qrcode.vue';
+import Card from '../components/layout/Card.vue';
+import Container from '../components/layout/Container.vue';
+import Button from '../components/base/buttons/Button.vue';
+
+export default defineComponent({
+  name: 'Signin',
+  components: {
+    Card,
+    Container,
+    Button,
+    Qrcode,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.card-wrapper {
+  width: 300px;
+  margin: 0 auto;
+
+  .card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+}
+</style>
