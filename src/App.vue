74198583a88bed313ac1377a9218529d8f8ec438
Jip Fr <jipfrijlink@gmail.com>
2021-04-30 11:14:24 +0200

    Basic CSS start

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Header />
+  <router-view />
 </template>
+
+<script lang="ts">
+// Import Vue
+import { defineComponent } from 'vue';
+
+// Import components
+import Header from './components/layout/Header.vue';
+
+export default defineComponent({
+  components: {
+    Header,
+  },
+});
+</script>
