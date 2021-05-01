966128304bb7a5b38db1526b43ebec5b541c0074
Martijn Faber <martijn@fabtronics.nl>
2021-05-01 08:39:45 +0200

    feat: add a simple route transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <Header />
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
 </template>
+
+<style lang="scss" scoped>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease-out;
+}
+</style>
 
 <script lang="ts">
 // Import Vue
