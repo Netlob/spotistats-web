32df0f9975d63763a39b458e3942b501acdc5479
Martijn Faber <martijn@fabtronics.nl>
2021-05-01 10:48:48 +0200

    feat(artist): add the artist page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
 import User from '../views/User.vue';
 import Signin from '../views/Signin.vue';
+import Artist from '../views/Artist.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,6 +19,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/signin',
     name: 'Signin',
     component: Signin,
+  },
+  {
+    path: '/artist/:id',
+    name: 'Artist',
+    component: Artist,
   },
   {
     path: '/about',
---
+++ src/views/Artist.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <div
+      class="block"
+      :style="{
+        backgroundImage: 'url(https://i.scdn.co/image/5a141b90473118fe09f078af76a9e0404da58335)',
+      }"
+    >
+      <h1 class="artist-name">Astrid S</h1>
+    </div>
+    <main>
+      <Container>
+        <Card>
+          <div>
+            <h2 class="card-title"><strong>Top tracks</strong></h2>
+            <ImageCardContainer>
+              <ImageCard
+                image="https://images-na.ssl-images-amazon.com/images/I/71bASkE2SSL._SL1200_.jpg"
+                title="Cherokee"
+                sticky-title="#5"
+                link="/artist/geenideewieditis"
+              />
+              <ImageCard
+                image="https://i0.wp.com/abitofpopmusic.com/wp-content/uploads/2020/06/astrid-s-dance-dance-dance.jpg?fit=1200%2C1200&ssl=1"
+                title="Dance Dance Dance"
+              />
+              <ImageCard
+                image="https://upload.wikimedia.org/wikipedia/en/2/26/Dark_Was_the_Night_%28Front_Cover%29.png"
+                title="So Far Around The Bend"
+              />
+              <ImageCard
+                image="https://images-na.ssl-images-amazon.com/images/I/71bASkE2SSL._SL1200_.jpg"
+                title="Cherokee"
+                sticky-title="#5"
+                link="/artist/geenideewieditis"
+              />
+              <ImageCard
+                image="https://i0.wp.com/abitofpopmusic.com/wp-content/uploads/2020/06/astrid-s-dance-dance-dance.jpg?fit=1200%2C1200&ssl=1"
+                title="Dance Dance Dance"
+              />
+              <ImageCard
+                image="https://upload.wikimedia.org/wikipedia/en/2/26/Dark_Was_the_Night_%28Front_Cover%29.png"
+                title="So Far Around The Bend"
+              />
+              <ImageCard
+                image="https://upload.wikimedia.org/wikipedia/en/2/26/Dark_Was_the_Night_%28Front_Cover%29.png"
+                title="So Far Around The Bend"
+              />
+            </ImageCardContainer>
+          </div>
+        </Card>
+      </Container>
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.block {
+  width: 100vw;
+  min-height: 500px;
+  background-color: var(--body-lighter);
+  border-radius: 0 0 40px 40px;
+  padding-top: 80px;
+  padding-bottom: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .artist-name {
+    font-size: 6rem;
+    text-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  }
+}
+
+main {
+  margin-top: 30px;
+}
+
+.card-title {
+  font-weight: inherit;
+  font-size: 1.5rem;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Container from '../components/layout/Container.vue';
+import ImageCard from '../components/base/ImageCard.vue';
+import ImageCardContainer from '../components/layout/ImageCardContainer.vue';
+import Card from '../components/layout/Card.vue';
+
+export default defineComponent({
+  name: 'Artist',
+  components: {
+    Container,
+    ImageCard,
+    ImageCardContainer,
+    Card,
+  },
+});
+</script>
