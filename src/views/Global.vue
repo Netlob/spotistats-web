b3474a79cf7a5cc6caa384200e2a83d1dd53e659
Martijn Faber <martijn@fabtronics.nl>
2021-05-02 09:45:04 +0200

    improvement(global): add box shadow to image cards

--- src/views/Global.vue
+++ src/views/Global.vue
@@ -67,6 +67,9 @@
   .image-card {
     width: 300px;
     height: 300px;
+    -webkit-box-shadow: 0px 10px 50px -10px rgba(0, 0, 0, 1);
+    -moz-box-shadow: 0px 10px 50px -10px rgba(0, 0, 0, 1);
+    box-shadow: 0px 10px 50px -10px rgba(0, 0, 0, 1);
   }
 
   .card1 {
