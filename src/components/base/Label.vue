d9a75aa271e552d959131e6e5c72c6eb296ea02d
Martijn Faber <martijn@fabtronics.nl>
2021-05-03 17:41:57 +0200

    feat(label): add label component

---
+++ src/components/base/Label.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="label">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.label {
+  width: max-content;
+  height: 50px;
+  background-color: var(--body-lighter);
+  border-radius: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  font-weight: 700;
+}
+</style>
