435bb0e627cd7cea26fc98d1383e27e43fa21473
Martijn Faber <martijn@fabtronics.nl>
2021-05-07 16:55:37 +0200

    feat(vuex): add global state for artists

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,11 +5,15 @@ const token = localStorage.getItem("token") || '';
 export default createStore({
   state: {
     tracks: [],
+    artists: []
   },
   mutations: {
     SET_TRACKS(state, tracks) {
-        state.tracks = tracks
+      state.tracks = tracks
     },
+    SET_ARTISTS(state, artists) {
+      state.artists = artists
+    }
   },
   actions: {
     async getTracks({ commit }) {
@@ -22,7 +26,19 @@ export default createStore({
         });
 
         const json = await tracks.json();
-        commit('SET_TRACKS', json)
+        commit('SET_TRACKS', json.items)
+    },
+    async getArtists({ commit }) {
+        const tracks = await fetch(`${process.env.VUE_APP_SERVER_URL}/spotify/me/top/artists?limit=50`, {
+            method: "GET",
+            headers: {
+            "Content-Type": "application/json",
+            Authorization: token,
+            },
+        });
+
+        const json = await tracks.json();
+        commit('SET_ARTISTS', json.items)
     },
   },
 });
