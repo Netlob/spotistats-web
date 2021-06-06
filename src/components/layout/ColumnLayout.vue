5d46f530b31d919bb9fad5984a47537538a4391c
Martijn Faber <martijn@fabtronics.nl>
2021-06-06 09:32:00 +0200

    feat(components): add columnlayout and column component

---
+++ src/components/layout/ColumnLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cols">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.cols {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: var(--gap);
+}
+</style>
