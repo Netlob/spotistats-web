2044eba2e21208ac0de5882335959f09117b2391
Martijn Faber <martijn@fabtronics.nl>
2021-06-06 17:13:38 +0200

    feat: add socialButton component

---
+++ src/components/base/buttons/SocialButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="social-button" @click.prevent="(e) => $emit('click', e)">
+    <span class="md">{{ icon }}</span>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.social-button {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: transparent;
+  border: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SocialButton',
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+  },
+});
+</script>
